a184b35cf170b606a60186a90a8661c75a8f7069
Rob <63966762+jesusarchangel@users.noreply.github.com>
2026-03-18 10:38:22 +0300

    feat: add Code Connect mapping for SegmentedButton

---
+++ src/components/SegmentedButton/SegmentedButton.figma.tsx
@@ -0,0 +1,30 @@
+import figma from '@figma/code-connect';
+import React from 'react';
+import { SegmentedButton } from './SegmentedButton';
+
+/**
+ * Figma Code Connect for SegmentedButton
+ * Figma node: https://www.figma.com/design/yfulxIdI8ysNiDrPwMRAWS/Segmented-button?node-id=81-633
+ */
+figma.connect(
+  SegmentedButton,
+  'https://www.figma.com/design/yfulxIdI8ysNiDrPwMRAWS/Segmented-button?node-id=81-633',
+  {
+    props: {
+      options: figma.nestedProps('.inner component / Button', {
+        value: figma.string('Text'),
+        label: figma.string('Text'),
+      }),
+    },
+    example: ({ options }) => (
+      <SegmentedButton
+        options={[
+          { value: 'option1', label: 'Button' },
+          { value: 'option2', label: 'Button' },
+        ]}
+        value="option1"
+        onChange={(val) => console.log(val)}
+      />
+    ),
+  },
+);
